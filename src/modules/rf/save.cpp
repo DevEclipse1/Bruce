c783cf4f43438d0738ef6cadc56bec98f7536d71
DevEclipse <98752478+DevEclipse1@users.noreply.github.com>
2025-05-15 21:54:54 +0200

    fixed saving invalid durations that are too long

--- src/modules/rf/save.cpp
+++ src/modules/rf/save.cpp
@@ -1,64 +1,62 @@
 #include "save.h"
 
 bool rf_raw_save(RawRecording recorded) {
-    FS *fs;
-    String filename = "";
-
-    if (!getFsStorage(fs)) {
+    FS *fs = nullptr;
+    if (!getFsStorage(fs) || fs == nullptr) {
         displayError("No space left on device", true);
         return false;
     }
 
-    String subfile_out = "Filetype: Bruce SubGhz File\nVersion 1\n";
-    subfile_out += "Frequency: " + String(int(recorded.frequency * 1000000)) + "\n";
-    subfile_out += "Preset: 0\n";
-    subfile_out += "Protocol: RAW\n";
+    if (!fs->exists("/BruceRF")) fs->mkdir("/BruceRF");
 
-    String rawDataLine = "RAW_Data: ";
-    for (size_t i = 0; i < recorded.codes.size(); ++i) {
-        // We should add a RAW_Data line for each code,
-        // but since the .sub file emitter adds too long of a delay between codes,
-        // we fit as many codes as possible in lines of up to 50k characters.
-        // More than 50k characters per line seems to cause file corruption.
-        if (rawDataLine.length() > 50000) {
-            subfile_out += rawDataLine;
-            subfile_out += "\n";
-            rawDataLine = "RAW_Data: ";
-        }
-        for (size_t j = 0; j < recorded.codeLengths[i]; ++j) {
-            if (recorded.codes[i][j].duration0 > 0) {
-                if (recorded.codes[i][j].level0 != 1) rawDataLine += "-";
-                rawDataLine += String(recorded.codes[i][j].duration0) + " ";
-            }
-            if (recorded.codes[i][j].duration1 > 0) {
-                if (recorded.codes[i][j].level1 != 1) rawDataLine += "-";
-                rawDataLine += String(recorded.codes[i][j].duration1) + " ";
-            }
-        }
-        if (i < recorded.codes.size() - 1) { rawDataLine += String(recorded.gaps[i] * -1000) + " "; }
-    }
-    subfile_out += rawDataLine;
-    subfile_out += "\n";
-    rawDataLine = "";
-
-    filename = "raw_";
-
+    String filename;
     int i = 0;
-    File file;
+    do {
+        filename = "/BruceRF/" + String(i) + ".sub";
+        i++;
+    } while (fs->exists(filename));
 
-    if (!(*fs).exists("/BruceRF")) (*fs).mkdir("/BruceRF");
-    while ((*fs).exists("/BruceRF/" + filename + String(i) + ".sub")) i++;
-
-    file = (*fs).open("/BruceRF/" + filename + String(i) + ".sub", FILE_WRITE);
-
-    if (file) {
-        file.println(subfile_out);
-        displaySuccess("/BruceRF/" + filename + String(i) + ".sub");
-    } else {
-        displayError("Error saving file", true);
+    File file = fs->open(filename, FILE_WRITE);
+    if (!file) {
+        displayError("Error creating file", true);
+        return false;
     }
 
-    file.close();
+    file.println("Filetype: Flipper SubGhz RAW File");
+    file.println("Version: 1");
+    file.printf("Frequency: %d\n", int(recorded.frequency * 1000000));
+    file.println("Preset: 0");
+    file.println("Protocol: RAW");
 
+    file.print("RAW_Data: ");
+
+    for (size_t i = 0; i < recorded.codes.size(); ++i) {
+        for (size_t j = 0; j < recorded.codeLengths[i]; ++j) {
+            int d0 = recorded.codes[i][j].duration0;
+            int d1 = recorded.codes[i][j].duration1;
+
+            if (d0 < 1000000 && d1 < 1000000 && d0 > -1000000 && d1 > -1000000) {
+                if (d0 > 0) {
+                    if (recorded.codes[i][j].level0 != 1) file.print("-");
+                    file.print(d0);
+                    file.print(" ");
+                }
+                if (d1 > 0) {
+                    if (recorded.codes[i][j].level1 != 1) file.print("-");
+                    file.print(d1);
+                    file.print(" ");
+                }
+            }
+        }
+
+        if (i < recorded.codes.size() - 1) {
+            file.print(recorded.gaps[i] * -1000);
+            file.print(" ");
+        }
+    }
+
+    file.println();
+    file.close();
+    displaySuccess(filename);
     return true;
 }
